8b25f63b7554460d21651d018f5815eab6b094df
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-11-30 20:04:26 +0900

    xpathを使った取得方法を学んだ

--- 04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
+++ 04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
@@ -2,7 +2,7 @@ import { chromium } from "@playwright/test";
 
 (async () => {
 	// ブラウザを起動
-	const browser = await chromium.launch({ headless: false, slowMo: 500 })
+	const browser = await chromium.launch({ headless: false, slowMo: 100 })
 
 	// 新規のページを作成
 	const page = await browser.newPage()
@@ -11,10 +11,15 @@ import { chromium } from "@playwright/test";
 	const pageTitle = await pageTitleLocator.innerText()
 	console.log('pageText', pageTitle);
 
+	// テキストで取得
 	const pageTextLocator = page.locator("text=名刺管理アプリ")
 	const pageText = await pageTextLocator.innerText()
 	console.log('pageText', pageText)
 
+	// xpathで取得
+	const xpathTextLocator = page.locator("xpath=//*[@id=\"__next\"]/nav/div/a")
+	const xpathText = await xpathTextLocator.innerText()
+	console.log('xpathText', xpathText)
 
 	await browser.close()
 })()
