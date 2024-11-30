0d3a215ab9676ebf199abb7e9a2c68c589f60208
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-11-30 19:40:32 +0900

    nodejsでブラウザを起動する方法、指定URLのHTML

--- 04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
+++ 04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
@@ -1,15 +1,16 @@
 import { chromium } from "@playwright/test";
-// console.log('chromium', chromium)
 
-// ブラウザのタブを起動
-const browser = await chromium.launch({ headless: false })
-// console.log('browser', browser)
+(async () => {
+	// ブラウザを起動
+	const browser = await chromium.launch({ headless: false, slowMo: 500 })
 
-// 新規のページを作成
-const page = await browser.newPage()
-await page.goto('http://localhost:3000')
+	// 新規のページを作成
+	const page = await browser.newPage()
+	await page.goto('http://localhost:3000')
 
-// ページのHTML情報を取得
-const htmlStr = await page.content()
-console.log(htmlStr)
+	// ページのHTML情報を取得
+	const htmlStr = await page.content()
 
+	// ブラウザを閉じる
+	browser.close()
+})
