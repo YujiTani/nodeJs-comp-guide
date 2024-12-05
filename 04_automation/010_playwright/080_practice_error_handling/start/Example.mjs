a362055fc33811292921ffe381a5830556972905
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-12-05 09:23:09 +0900

    条件に一致する役職の情報を取得する方法を練習した

--- 04_automation/010_playwright/080_practice_error_handling/start/Example.mjs
+++ 04_automation/010_playwright/080_practice_error_handling/start/Example.mjs
@@ -1,4 +1,6 @@
 import { chromium } from "@playwright/test";
+import * as fs from 'fs'
+import { Parser } from "json2csv";
 
 /**
  * 練習問題
@@ -10,8 +12,54 @@ import { chromium } from "@playwright/test";
  */
 (async () => {
   const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto(process.env.TARGET_URL);
+  const page = await browser.newPage()
+  await page.goto('http://localhost:3000')
+
+  await page.waitForTimeout(1000)
+
+  // 3ページ目に移動する
+  const page3Locator = page.locator('.page-link.page-number >> nth=2')
+  await page3Locator.click()
+
+  const cardLocators = page.locator('.cards.list-group-item')
+  const cardCount = await cardLocators.count()
+
+  const fetchedCards = []
+
+  for (let i = 0; i < cardCount; i++) {
+    const cardLocator = cardLocators.locator(`nth=${i} >> a`);
+    await cardLocator.click();
+
+    const divisionLocator = page.locator('.division')
+    const rollText = await divisionLocator.textContent()
+
+    if (!rollText.includes('係長')) {
+      const backLocator = page.locator('text=戻る')
+      await backLocator.click()
+      continue
+    }
+
+    let companyText = ''
+    try {
+      const companyLocator = page.locator('.card-title.company')
+      const companyText = await companyLocator.textContent()
+    } catch { }
+
+    const nameLocator = page.locator('.card-text.name')
+    const nameText = await nameLocator.textContent()
+
+    fetchedCards.push({
+      company: companyText,
+      name: nameText
+    })
+
+    const backLocator = page.locator('text=戻る')
+    await backLocator.click()
+  }
 
   await browser.close();
+  const parser = new Parser()
+  const csv = await parser.parse(fetchedCards)
+  fs.writeFileSync("./text-data.csv", csv);
 })();
+
